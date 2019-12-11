376de4a92476d830a95ecee898bf0186e6f3bfe1
Asaf Gal <asafg1@gmail.com>
2019-12-11 11:22:01 -0500

    mac bin

--- matlab/misc/compile_antrax_executables.m
+++ matlab/misc/compile_antrax_executables.m
@@ -36,23 +36,9 @@ eval(['mcc -m antrax.mlapp  -a ',srcdir,' -d ',bindir, ' -o ', prefix, 'app'])
 
 % compile the track function
 report('I','    ...track function')
-eval(['mcc -m track_single_movie.m  -a ',srcdir,' -d ',bindir, ' -o ', prefix, 'track_batch'])
+eval(['mcc -m track_single_movie.m  -a ',srcdir,' -d ',bindir, ' -o ', prefix, 'track_single_movie'])
+eval(['mcc -m link_across_movies.m  -a ',srcdir,' -d ',bindir, ' -o ', prefix, 'link_across_movies'])
 
 % compile the solve function
 report('I','    ...solve function')
 
-if strcmp(computer,'GLNXA64')
-    
-    % compile the hpc track function (linux only)
-    report('I','    ...hpc track function')
-    eval(['mcc -m track_single_movie_on_cluster.m  -a ',srcdir,' -d ',bindir, ' -o ', prefix, 'hpc_track_single'])
-    
-    % compile the hpc solve function (linux only)
-    report('I','    ...hpc solve function')
-    eval(['mcc -m solve_single_graph_on_cluster.m  -a ',srcdir,' -d ',bindir, ' -o ', prefix, 'hpc_solve_single'])
-    
-else
-    
-    report('W', 'Not on Linux computer, skipping hpc functions')
-
-end
