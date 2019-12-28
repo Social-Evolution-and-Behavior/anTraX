4109604e48a8dc0e04b2a565bce44a71b62f5e2a
Asaf Gal <asafg1@gmail.com>
2019-12-28 18:41:07 -0500

    Update run_antrax.py

--- scripts/run_antrax.py
+++ scripts/run_antrax.py
@@ -39,8 +39,6 @@ def parse_explist(exparg):
 
     exps = []
 
-    print('parsing ' + exparg)
-
     if isdir(exparg) and is_expdir(exparg):
         exps.append(axExperiment(exparg))
     elif isfile(exparg):
