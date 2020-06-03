4445c7a35cbabeea770757566b40deb458c296c2
Asaf Gal <asafg1@gmail.com>
2020-06-03 16:32:20 -0400

    Update antrax_expdir_reorg.py
    hpc reorg

--- scripts/antrax_expdir_reorg.py
+++ scripts/antrax_expdir_reorg.py
@@ -4,13 +4,13 @@ from glob import glob
 from clize import run
 from threading import Thread
 from shutil import copyfile, copy, rmtree, copytree
-from subprocess import Popen
+from subprocess import Popen, PIPE
 import queue
 import time
 import antrax as ax
 
 
-def reorg(expdir, targetdir, *, new_expname=None, missing=False, force=False, tracking=False, nw=1):
+def reorg(expdir, targetdir, *, new_expname=None, missing=False, force=False, tracking=False, nw=1, hpc=False):
 
     expname = [x for x in expdir.split('/') if len(x) > 0][-1]
 
@@ -71,27 +71,55 @@ def reorg(expdir, targetdir, *, new_expname=None, missing=False, force=False, tr
         if not isfile(x):
             copyfile(v, x)
 
-    q = queue.Queue()
+    # copy dat files
+    ax.report('I', 'Copying dat files')
+    for v, vnew in zip(videos, new_videos):
+        dat = v[:-3] + 'dat'
+        new_dat = vnew[:-3] + 'dat'
+        if isfile(dat) and not isfile(new_dat):
+            copyfile(dat, new_dat)
 
-    for v, newv in zip(videos, new_videos):
-        w = {'vid': v, 'new_vid': newv}
-        q.put_nowait(w)
+    if not hpc:
 
-    for _ in range(nw):
-        Worker(q).start()
+        q = queue.Queue()
 
-    q.join()  # blocks until the queue is empty.
+        for v, newv in zip(videos, new_videos):
+            w = {'vid': v, 'new_vid': newv}
+            q.put_nowait(w)
 
-    '''
-    # loop on videos
-    tot = len(videos)
-    cnt = 1
-    
-    for v, newv in zip(videos, new_videos):
-        ax.report('I', 'Working on file ' + str(cnt) + '/' + str(tot))
-        do_one_video(v, newv)
-        cnt += 1
-    '''
+        for _ in range(nw):
+            Worker(q).start()
+
+        q.join()  # blocks until the queue is empty.
+
+    else:
+
+        # create job file
+
+        jobfile = join(new_expdir, 'antrax_reorg.sh')
+        with open(jobfile, 'w') as f:
+
+            in_array_line = 'VIN=(' + ' '.join(videos) + ')'
+            out_array_line = 'VOUT=(' + ' '.join(new_videos) + ')'
+
+            cmd = 'ffmpeg -loglevel error  -i ${VIN[$SLURM_ARRAY_TASK_ID]} -vcodec libx264 -preset veryslow -crf 30 ${VOUT[$SLURM_ARRAY_TASK_ID]}'
+
+            f.writelines("#!/bin/bash\n")
+            f.writelines("#SBATCH --job-name=%s\n" % 'reorg')
+            f.writelines("#SBATCH --output=%s_%%a.log\n" % join(new_expdir, 'antrax_reorg.log'))
+            f.writelines("#SBATCH --ntasks=%d\n" % 1)
+            f.writelines("#SBATCH --cpus-per-task=%d\n" % 8)
+            f.writelines("#SBATCH --array=%d-%d%%%d\n" % (0, len(new_videos)-1, 100))
+            f.writelines("\n")
+            f.writelines(in_array_line)
+            f.writelines(out_array_line)
+            f.writelines("srun -N1 %s\n" % cmd)
+
+        p = Popen("sbatch %s" % jobfile, stdout=PIPE, shell=True)
+        (out, err) = p.communicate()
+        out = out.decode("utf-8")
+        jid = out.split()[-1]
+        ax.report('I', 'Submitted job ' + str(jid))
 
     # copy tracking sessions
     if tracking:
@@ -124,10 +152,6 @@ class Worker(Thread):
 
                 return
 
-            # do whatever work you have to do on work
-            dat = w['vid'].replace('.avi', '.dat')
-            new_dat = w['new_vid'].replace('.mp4', '.dat')
-
             # compress the video to the new location
             if not isfile(w['new_vid']):
                 cmd = 'ffmpeg -loglevel error  -i ' + w['vid'] + ' -vcodec libx264 -preset veryslow -crf 30 ' + w['new_vid']
@@ -136,11 +160,6 @@ class Worker(Thread):
                 ax.report('I', 'Finished trancoding ' + w['new_vid'].split('/')[-1])
             else:
                 ax.report('I', 'Skipping ' + w['new_vid'].split('/')[-1])
-
-            # resave dat to new location
-            if isfile(dat) and not isfile(new_dat):
-                copyfile(dat, new_dat)
-
 
             self.q.task_done()
 
