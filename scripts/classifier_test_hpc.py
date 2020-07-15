53ef063f93600f80590fc31ce877c8786c9a3165
Asaf Gal <asafg1@gmail.com>
2020-07-15 17:20:03 -0400

    Create classifier_test_hpc.py

---
+++ scripts/classifier_test_hpc.py
@@ -0,0 +1,52 @@
+
+from subprocess import Popen, PIPE
+
+
+NS = [10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200]
+NE = 300
+
+HOME = '/ru-auth/local/home/agal/'
+CLASSDIR = HOME + '/scratch/Classifiers/A36_classifier_analysis'
+WD = HOME + '/antrax_classifer_test/'
+LOGDIR = HOME + 'logs/'
+LOGFILE = WD + 'num_examples_test.log'
+TEMPLATE = HOME + '/code/anTraX/scripts/classifier_test_jobfile.sh'
+JOBFILE = WD + 'jobfile.sh'
+NREP = 50
+
+if __name__ == '__main__':
+
+    for n in NS:
+
+        # create jobfile
+        with open(JOBFILE, 'w') as f:
+
+            name = 'n' + str(n)
+
+            f.writelines("#!/bin/bash\n")
+            f.writelines("#SBATCH --job-name=%s\n" % name)
+            f.writelines("#SBATCH --output=%s_%%a.log\n" % LOGDIR + 'n' + str(n) + '_%a')
+            f.writelines("#SBATCH --ntasks=%d\n" % 1)
+            f.writelines("#SBATCH --cpus-per-task=%d\n" % int(8))
+            f.writelines("#SBATCH --array=%d-%d%%%d\n" % (1, NREP, 10))
+
+            f.writelines("#SBATCH --mail-type=ALL\n")
+            f.writelines("#SBATCH --mail-user=agal@rockefeller.edu%s\n")
+            f.writelines("\n")
+
+            cmd = 'python /ru-auth/local/home/agal/code/anTraX/scripts/classifier_test.py ' + \
+                CLASSDIR + \
+                ' -n ' + str(n) + \
+                ' --name ' + name + \
+                ' --ne ' + str(NE) + \
+                ' --logfile ' + LOGFILE
+
+            f.writelines("srun -N1 %s\n" % cmd)
+
+            p = Popen("sbatch %s" % JOBFILE, stdout=PIPE, shell=True)
+
+            (out, err) = p.communicate()
+            print(out)
+            out = out.decode("utf-8")
+            jid = out.split()[-1]
+
